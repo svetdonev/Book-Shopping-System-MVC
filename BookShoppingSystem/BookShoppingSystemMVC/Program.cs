6f82cc18830363ba939cb360551c86d607d51008
Svetlozar Donev <svetdonev1@gmail.com>
2024-09-10 17:42:16 +0300

    Clean up program.cs

--- BookShoppingSystem/BookShoppingSystemMVC/Program.cs
+++ BookShoppingSystem/BookShoppingSystemMVC/Program.cs
@@ -2,11 +2,9 @@ using BookShoppingSystemMVC.Data;
 using BookShoppingSystemMVC.Infrastructure;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<BookSystemDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -21,7 +19,6 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 await app.PrepareDatabase();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -29,7 +26,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
